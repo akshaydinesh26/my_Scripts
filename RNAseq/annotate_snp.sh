ff501715d3c0c0c97f9550532130b9d4da822738
akshaydinesh26 <53232139+akshaydinesh26@users.noreply.github.com>
2023-03-09 19:34:05 +0530

    Delete annotate_snp.sh

--- RNAseq/annotate_snp.sh
+++
@@ -1,7 +0,0 @@
-#peprare gene.bed from gtf file using gff2bed
-#gene.bed should have
-#chr	Gene-start	Gene-end	Gene-symbol	Entrez-Gene ID	Gene-Description	Gene-biotype
-for f in `ls *.vcf | sed 's/.vcf//' | sort -u` ; do 
-	awk '{print $1"\t"$2"\t"$2"\t"$4"\t"$5}' ${f}.vcf | grep "#" -v > ${f}snp.bed
-	bedtools window -a ${f}snp.bed -b genes.bed -l 0 -r 0 > ${f}_anno.bed
-done
