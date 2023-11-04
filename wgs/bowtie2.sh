3a6979afada8546b385c354faa5e74c90b36bcac
akshaydinesh26 <53232139+akshaydinesh26@users.noreply.github.com>
2023-11-04 08:20:20 +0530

    Delete wgs directory

--- wgs/bowtie2.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/bash
-file=($(ls *_1.fq.gz | sed 's/_1.fq.gz//g' | sed 's/filtered_//g'));
-
-for var in ${file[@]}; do\
- echo "doing for ${var}"
- bowtie2 -p 10 -x /media/bionivid-256/Data_256/New_data/WGS/2023/Dr.Stella/1_ref/Apidx\
- -1 "filtered_${var}_1.fq.gz" -2 "filtered_${var}_2.fq.gz" | samtools view -@ 10\
- -bS -o "../3_alignment/${var}.bam";
- done
