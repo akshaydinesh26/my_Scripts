67df43b2a75ae12c7ede35f8799b96a7d1a22525
akshaydinesh26 <53232139+akshaydinesh26@users.noreply.github.com>
2023-12-18 13:37:34 +0530

    Update bowtie2.sh

--- bioinformatics/bowtie2.sh
+++ bioinformatics/bowtie2.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
+
+#execute as ./bowtie2.sh index outputfolder
 file=($(ls *_1.fq.gz | sed 's/_1.fq.gz//g' | sed 's/filtered_//g'));
+index=$1
+out=$2
 
 for var in ${file[@]}; do\
  echo "doing for ${var}"
- bowtie2 -p 10 -x /media/bionivid-256/Data_256/New_data/WGS/2023/Dr.Stella/1_ref/Apidx\
+ bowtie2 -p 10 -x ${index}\
  -1 "filtered_${var}_1.fq.gz" -2 "filtered_${var}_2.fq.gz" | samtools view -@ 10\
- -bS -o "../3_alignment/${var}.bam";
+ -bS -o "${out}/${var}.bam";
  done
